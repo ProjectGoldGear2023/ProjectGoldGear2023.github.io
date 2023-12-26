f4c6d170c6d4d8abce993ee28656eb3b81255154
Maschinenmann <herceclase@gmail.com>
2023-12-27 00:04:47 +0100

    company image added to footer

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 let isScrollHintVisible = true;
 
+function changeImage(element) {
+    console.log(element.src)
+    if (element.src.includes('red_canvas')) {
+        element.style.height = '120px';
+        element.src = './assets/images/Picuezo_Studios_old.webp'
+    } else if (element.src.includes('old')) {
+        element.style.height = '100px';
+        element.src = './assets/images/Picuezo_Studios_new.webp'
+    } else {
+        element.style.height = '64px';
+        element.src = './assets/images/red_canvas.webp';
+    }
+}
+
 // SCROLL METHODS
 function controllScrollHint() {
     if (window.scrollY >= 100 && isScrollHintVisible === true) {
